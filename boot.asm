63a2af904e3f17994cde8e36e277052648b2d31f
philipportner <philipp.ortner@student.tugraz.at>
2021-02-22 15:33:56 +0100

    32bit mode

--- boot.asm
+++ boot.asm
@@ -2,14 +2,58 @@ bits 16 ; tell NASM this is 16bit code
 org 0x7c00 ; tell NASM to start outputting stuff at 0x7c00
 
 boot:
-    mov si,hello ; point si register to hello label
-    mov ah,0x0e ; 0x0e means 'write char in TTY mode'
+    mov ax, 0x2401
+    int 0x15 ; call interrupt 0x15, enables A20 bit
+    mov ax, 0x3
+    int 0x10 ; vga text mode 3
+    lgdt [gdt_pointer] ; loads gdt table
+    mov eax , cr0
+    or eax, 0x1 ; set protected mode bit on control register 0
+    mov cr0, eax
+    jmp CODE_SEG:boot2 ; long jump to code segment
+
+gdt_start:
+    dq 0x0
+gdt_code:
+    dw 0xFFFF
+    dw 0x0
+    db 0x0
+    db 10011010b
+    db 11001111b
+    db 0x0
+gdt_data:
+    dw 0xFFFF
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+gdt_end:
+
+gdt_pointer:
+    dw gdt_end - gdt_start
+    dd gdt_start
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+bits 32 ; 32 bit mode
+boot2:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esi,hello
+    mov ebx,0xb8000
 
 .loop:
     lodsb
     or al,al ; is al == 0 ?
     jz halt ; if al == 0 jmp to halt
-    int 0x10 ; runs BIOS interrupt 0x10 - video services
+    or eax, 0x0100
+    mov word [ebx], ax
+    add ebx, 2
     jmp .loop
 
 halt:
